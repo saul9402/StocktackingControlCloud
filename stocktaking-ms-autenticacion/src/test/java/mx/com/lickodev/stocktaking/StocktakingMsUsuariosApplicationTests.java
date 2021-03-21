a33d15ebf40979ec4fb50de10a9fc13e0d40b0c3
Saul Avila <saul.avila@nyrhcloud.com>
2021-03-20 20:58:02 -0600

    Se agrega proyecto commons en proyecto autenticacion

--- stocktaking-ms-autenticacion/src/test/java/mx/com/lickodev/stocktaking/StocktakingMsUsuariosApplicationTests.java
+++ stocktaking-ms-autenticacion/src/test/java/mx/com/lickodev/stocktaking/StocktakingMsUsuariosApplicationTests.java
@@ -3,7 +3,7 @@ package mx.com.lickodev.stocktaking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = { StocktakingMsUsuariosApplication.class })
 class StocktakingMsUsuariosApplicationTests {
 
 	@Test
