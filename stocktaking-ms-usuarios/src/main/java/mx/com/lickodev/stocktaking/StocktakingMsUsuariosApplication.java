faf36942d6190f78ffeec79316fdad7971660de1
Saul Avila <saul.avila@nyrhcloud.com>
2021-03-20 21:34:54 -0600

    Se agregan configuraciones para el proyecto de usuarios

--- stocktaking-ms-usuarios/src/main/java/mx/com/lickodev/stocktaking/StocktakingMsUsuariosApplication.java
+++ stocktaking-ms-usuarios/src/main/java/mx/com/lickodev/stocktaking/StocktakingMsUsuariosApplication.java
@@ -1,9 +1,15 @@
 package mx.com.lickodev.stocktaking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"mx.com.lickodev.stocktaking.entity"})
+@EnableEurekaClient
+@EnableAutoConfiguration
 public class StocktakingMsUsuariosApplication {
 
 	public static void main(String[] args) {
