e5cc0f2cfb15a7fc0dde39bf07fead3b26e4b4d3
Saul Avila <saul.avila@nyrhcloud.com>
2021-04-08 00:01:32 -0500

    Se agrega configuracion para tests

--- stocktaking-zuul-server/src/test/java/mx/com/lickodev/stocktaking/StocktakingZuulServerApplicationTests.java
+++ stocktaking-zuul-server/src/test/java/mx/com/lickodev/stocktaking/StocktakingZuulServerApplicationTests.java
@@ -3,7 +3,9 @@ package mx.com.lickodev.stocktaking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import mx.com.lickodev.stocktaking.zuul.StocktakingZuulServerApplication;
+
+@SpringBootTest(classes = { StocktakingZuulServerApplication.class })
 class StocktakingZuulServerApplicationTests {
 
 	@Test
